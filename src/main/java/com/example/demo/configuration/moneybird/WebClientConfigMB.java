9ff95998e9cd1189bdd38a065e3e7ee475a61b8b
Vitali Bestolkau <bestolkovv@gmail.com>
2023-03-28 12:29:44 +0200

    feat: add FilterChain for Moneybird

--- src/main/java/com/example/demo/configuration/moneybird/WebClientConfigMB.java
+++ src/main/java/com/example/demo/configuration/moneybird/WebClientConfigMB.java
@@ -3,8 +3,12 @@ package com.example.demo.configuration.moneybird;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -21,5 +25,20 @@ public class WebClientConfigMB {
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain moneybirdFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/moneybird/**").permitAll()
+                        // TODO: May come up with more strict rules for people accessing other URLs
+                )
+                .csrf()
+                .disable();
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+        return http.build();
     }
 }
